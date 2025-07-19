25c6464efdd717a45de547662ae9cdd1a49ed8b2
Dafei Qin <qindafei@connect.hku.hk>
2025-07-19 15:39:22 +0800

    update

--- occwl_offline_render.py
+++ occwl_offline_render.py
@@ -10,7 +10,8 @@ def render_solid(solid, output_file_path):
     v.fit()
     v.save_image(output_file_path)
 
-v = OffscreenRenderer()
-solids = load_step(r"C:\Users\Dafei Qin\Documents\WORK\CAD\data\examples\00000056\00000056_666139e3bff64d4e8a6ce183_step_005.step")
-for idx, solid in enumerate(solids):
-    render_solid(solid, rf"C:\Users\Dafei Qin\Documents\WORK\CAD\data\examples\00000056\{idx:03d}.png")
+
+if __name__ == "__main__":
+    solids = load_step(r"C:\Users\Dafei Qin\Documents\WORK\CAD\data\examples\00000056\00000056_666139e3bff64d4e8a6ce183_step_005.step")
+    for idx, solid in enumerate(solids):
+        render_solid(solid, rf"C:\Users\Dafei Qin\Documents\WORK\CAD\data\examples\00000056\{idx:03d}.png")
