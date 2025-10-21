73b11980803547d6e091c0e0dbc89820df232d1f
Dafei Qin <qindafei@connect.hku.hk>
2025-10-21 16:47:01 +0800

    add_test_vae_v1

---
+++ src/tests/test_vae_v1.py
@@ -0,0 +1,81 @@
+import torch
+import numpy as np
+import sys
+sys.path.append('/home/qindafei/CAD/CLR-Wire')
+
+from src.dataset.dataset_v1 import dataset_compound, SURFACE_TYPE_MAP, SCALAR_DIM_MAP
+from src.vae.vae_v1 import SurfaceVAE
+from src.utils.numpy_tools import orthonormal_basis_from_normal
+
+from utils.surface import visualize_json_interset
+
+def to_json(params_tensor, types_tensor, mask_tensor):
+    json_data = []
+    SURFACE_TYPE_MAP_INVERSE = {value: key for key, value in SURFACE_TYPE_MAP.items()}
+    for i in range(len(params_tensor)):
+        params = params_tensor[i][mask_tensor[i]]
+        surface_type = SURFACE_TYPE_MAP_INVERSE[types_tensor[i].item()]
+        assert params.shape[0] == 10 + SCALAR_DIM_MAP[surface_type]
+        scalar = params[10:]
+        direction = params[3:6]
+        N, X, Y = orthonormal_basis_from_normal(direction.numpy())
+
+        surface_data = {
+            'type': surface_type,
+            'idx': i,
+            'location': params[:3].numpy(),
+            'direction': np.array([N, X, Y]).tolist(),
+            'scalar': scalar.numpy(),
+            'poles': [],
+            'uv': params[6:10].numpy(),
+            "orientation": "Forward"
+
+        }
+
+        json_data.append(surface_data)
+
+    return json_data
+
+if __name__ == '__main__':
+
+    dataset = dataset_compound('/home/qindafei/CAD/data/logan_jsons/abc/0/0000')
+    params_tensor, types_tensor, mask_tensor = dataset[12]
+    print(params_tensor.shape)
+    print(types_tensor.shape)
+    print(mask_tensor.shape)
+
+    params_tensor = params_tensor[mask_tensor.bool()]
+    types_tensor = types_tensor[mask_tensor.bool()]
+
+
+
+    model = SurfaceVAE(param_raw_dim=[10, 11, 12, 12, 11])
+    checkpoint_path = '/home/qindafei/CAD/CLR-Wire/checkpoints/vae_v1_debug/model-26.pt'
+    checkpoint = torch.load(checkpoint_path, map_location="cpu")
+    if 'ema_model' in checkpoint:
+        ema_model = checkpoint['ema']
+        ema_model = {k.replace("ema_model.", ""): v for k, v in ema_model.items()}
+        model.load_state_dict(ema_model, strict=False)
+        print("Loaded EMA model weights for classification.")
+    elif 'model' in checkpoint:
+        model.load_state_dict(checkpoint['model'])
+        print("Loaded model weights for classification.")
+    else:
+        model.load_state_dict(checkpoint)
+        print("Loaded raw model state_dict for classification.")
+
+    # model.load_state_dict(torch.load(''))
+    model.eval()
+
+    mu, logvar = model.encode(params_tensor, types_tensor)
+    z = model.reparameterize(mu, logvar)
+    type_logits_pred, types_pred = model.classify(z)
+    params_pred, mask = model.decode(z, types_pred)
+
+    recon_fn = torch.nn.MSELoss()
+    recon_loss = (recon_fn(params_pred, params_tensor)) * mask.float().mean()
+    accuracy = (types_pred == types_tensor).float().mean()
+    print(f'recon_loss: {recon_loss.item()}, accuracy: {accuracy.item()}')
+
+    visualize_json_interset(to_json(params_pred, types_pred, mask), plot=True)
+    
